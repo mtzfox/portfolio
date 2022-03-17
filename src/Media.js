dbe51bbe0eca6422f45e35fed72e61b098fc7d80
Mike <21209421+mtzfox@users.noreply.github.com>
2022-03-17 03:25:46 -0700

    almost fixed react app

---
+++ src/Media.js
@@ -0,0 +1,13 @@
+// import { createMedia } from "@artsy/fresnel"
+
+// const AppMedia = createMedia({
+//   breakpoints: {
+//     mobile: 0,
+//     tablet: 768,
+//     computer: 1024,
+//   },
+// })
+// export const mediaStyle = AppMedia.createMediaStyle()
+// export const { Media, MediaContextProvider } = AppMedia
+// Generate CSS to be injected into the head
+// export const { Media, MediaContextProvider } = AppMedia
