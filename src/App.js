e45c34020461bcd77d2edbbf938a3f3ac526f44b
Mike <21209421+mtzfox@users.noreply.github.com>
2022-03-17 12:55:04 -0700

    fixed semantic install

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import './assets/fomantic/dist/semantic.css';
 //import { Container, Header } from 'semantic-ui-react';
 import './App.css';
@@ -10,6 +11,21 @@ function App() {
       </header>
     </div>
   );
+=======
+import React, { Component } from 'react'
+import 'fomantic-ui-css/semantic.css'
+import { Container, Header } from 'semantic-ui-react';
+class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Header>
+          Fomantic-UI & Semantic-UI-React
+        </Header>
+      </Container>
+    );
+  }
+>>>>>>> d6f08fb (fixed semantic install)
 }
 
 export default App;
