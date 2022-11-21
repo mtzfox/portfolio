bdd4585ccd7431a5a43720f878c3011bc8b60241
Mike <21209421+mtzfox@users.noreply.github.com>
2022-11-21 15:13:30 -0800

    merge

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 import './fonts.css';
 import './style.css';
 import 'semantic-ui-css/semantic.min.css';
 import 'semantic-ui-less/semantic.less';
 import App from './App';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
