4704f9e7f26ad30d83a2195e3fe93203b382f278
Mike <21209421+mtzfox@users.noreply.github.com>
2022-11-21 02:35:23 -0800

    moved files

--- src/components/ProjectGrid.js
+++ src/components/ProjectGrid.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Grid, Image } from 'semantic-ui-react'
 
-const cryptoImg = './assets/images/crypto-app.png';
+const cryptoImg = './public/assets/images/crypto-app.png'
 
 const ProjectGrid = () => (
     <Grid>
